f9c9ee806e10810d43bc942f9d22550930c16714
Saideekshitherram <156347661+Saideekshitherram@users.noreply.github.com>
2024-01-12 23:09:07 +0530

    Update dia.py

--- dia.py
+++ dia.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from tensorflow.keras.models import load_model
 
 # Load your dataset
 data = pd.read_csv('mlp.csv')
